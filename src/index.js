1993fbcc5b2f705d489e2221ec8c1d0d1d0c3fcb
CALVINCE <calvince.ogwel@gmail.com>
2022-12-06 21:40:12 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+let input = document.getElementById("entry")
+let button = document.getElementById("addList")
+let tasks = document.getElementById("tasks")
+button.addEventListener("click", () => {
+    let task = document.createElement("li")
+     task.textContent = input.value
+     tasks.appendChild(task)
+})
